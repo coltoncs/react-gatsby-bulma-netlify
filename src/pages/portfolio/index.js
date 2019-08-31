64db90488570a77aaf404175902cc695c8a7cac2
Colton Sweeney <colton.sweeney@gmail.com>
2019-08-30 22:53:15 -0400

    adjust images and responsiveness :iphone:

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -10,7 +10,8 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
+            backgroundImage: `url('/img/adobestock_180105378.jpeg')`,
+            backgroundPosition: `center center`,
           }}
         >
           <h1
