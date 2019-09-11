7b395eb303b1d424563fc112e824d95834557de9
Colton Sweeney <colton.sweeney@gmail.com>
2019-09-10 22:09:48 -0400

    fix env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS,
+        trackingId: process.env.GOOGLE_ID,
         head: true,
       },
     }, 
