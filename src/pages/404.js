b4b5f4ed760aedd8babdd9ff5e52d51b7a156e45
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 12:17:11 -0500

    fix thank you message for contact form

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="container no-route">
+    <div className="container no-route is_bigscreen">
       <h1>404 - PAGE NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... how embarrassing.</p>
     </div>
---
+++ src/pages/contact/thanks.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default () => (
+  <Layout>
+    <section className="section">
+      <div className="container is_bigscreen">
+        <div className="content">
+          <h1>Thank you!</h1>
+          <p>Your message has been sent. If I'm not currently at my computer, I'll get back to you as soon as I'm available.</p>
+          <br/>
+          <p>Thanks again!</p>
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
